4b440adda1a740040b4337f56c63e6b322bf3dfd
gudtn2 <tyrueu@gmail.com>
2023-03-14 13:45:58 +0900

    edu

--- My project/Assets/Scripts/BackGroundController.cs
+++ My project/Assets/Scripts/BackGroundController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BackGroundController : MonoBehaviour
 {
-    // ** 각각의 BackGround가 모여있는 계층구조의 최상위 객체(부모)
+    // ** BackGround 가 모여있는 계층구조의 최상위 객체(부모)
     private Transform parent;
 
     // ** Sprite를 포함하고 있는 구성요소
@@ -16,21 +16,23 @@ public class BackGroundController : MonoBehaviour
     // ** 생성지점
     private float endPoint;
 
-    // ** 삭제지점
+    // ** 삭제 지점.
     private float exitPoint;
 
-    // ** 이미지 이동 속도
+    // ** 이미지 이동속도
     public float Speed;
 
     // ** 플레이어 정보
     private GameObject player;
     private PlayerController playerController;
 
-    // **  움직임 정보
+    // ** 움직임 정보
     private Vector3 movemane;
 
-    // ** 이미지가 중앙 위치에 정상적으로 노충될 수 있도록 하기 위한 완충역할
+    // ** 이미지가 중앙 위치에 정상적으로 노출될 수 있도록 하기 위한 완충역할.
     private Vector3 offset = new Vector3(0.0f, 7.5f, 0.0f);
+
+    private GameObject Test;
 
     private void Awake()
     {
@@ -43,7 +45,7 @@ public class BackGroundController : MonoBehaviour
         // ** 현재 이미지를 담고있는 구성요소를 받아온다.
         spriteRenderer = GetComponent<SpriteRenderer>();
 
-        // ** 플레이어 이미지를 담고잇는 구성요소를 받아온다.
+        // ** 플레이어 이미지를 담고있는 구성요소를 받아온다.
         playerController = player.GetComponent<PlayerController>();
     }
 
@@ -52,35 +54,33 @@ public class BackGroundController : MonoBehaviour
         // ** 구성요소에 포함된 이미지를 받아온다.
         sprite = spriteRenderer.sprite;
 
-        // ** 시작지점을 설정
+        // ** 시작지점을 설정.
         endPoint = sprite.bounds.size.x * 0.5f + transform.position.x;
 
-        // ** 종료지점을 설정
+        /*
+        Test = new GameObject("Gozmo");
+        Test.AddComponent<MyGizmo>();
+        Test.transform.position = new Vector3(endPoint, 0.0f, 0.0f);
+         */
+
+
+        // ** 종료지점을 설정.
         exitPoint = -(sprite.bounds.size.x * 0.5f) + player.transform.position.x;
     }
 
     void Update()
     {
         // ** 이동정보 셋팅
-        movemane = new Vector3(Input.GetAxisRaw("Horizontal") * Time.deltaTime * Speed + offset.x, player.transform.position.y + offset.y, 0.0f + offset.z);
+        movemane = new Vector3(
+            Input.GetAxisRaw("Horizontal") * Time.deltaTime * Speed + offset.x,
+            0.0f, 0.0f);
 
-        // ** 플레이어가 바라보고 있는 방향에 따라 분기됨
-
-        if (ControllerManager.GetInstance().DirLeft)
-        {// ** 좌측 이동
-         // ** 이동정보 적용
-            
+        // ** singleton
+        if (ControllerManager.GetInstance().DirRight)
+        {
+            transform.position -= movemane;
             endPoint -= movemane.x;
         }
-
-        if (ControllerManager.GetInstance().DirRight)
-        {// ** 우측 이동
-                transform.position -= movemane;          
-        }        
-
-        // ** 이동정보 적용
-        
-       
 
         // ** 동일한 이미지 복사
         if (player.transform.position.x + (sprite.bounds.size.x * 0.5f) + 1 > endPoint)
@@ -88,14 +88,16 @@ public class BackGroundController : MonoBehaviour
             // ** 이미지를 복제한다.
             GameObject Obj = Instantiate(this.gameObject);
 
-            // ** 복제된 이미지의 부모를 설정한다
+            // ** 본제된 이미지의 부모를 설정한다.
             Obj.transform.parent = parent.transform;
 
             // ** 복제된 이미지의 이름을 설정한다.
             Obj.transform.name = transform.name;
 
-            // ** 복제된 이미지 위치를 설정한다.
-            Obj.transform.position = new Vector3(endPoint + 25.0f, 0.0f, 0.0f);
+            // ** 복제된 이미지의 위치를 설정한다.
+            Obj.transform.position = new Vector3(
+                endPoint + 25.0f,
+                0.0f, 0.0f);
 
             // ** 시작지점을 변경한다.
             endPoint += endPoint + 25.0f;
@@ -103,9 +105,6 @@ public class BackGroundController : MonoBehaviour
 
         // ** 종료지점에 도달하면 삭제한다.
         if (transform.position.x + (sprite.bounds.size.x * 0.5f) - 2 < exitPoint)
-        {
             Destroy(this.gameObject);
-        }
-
     }
 }
--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
+
+
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,103 +12,97 @@ public class PlayerController : MonoBehaviour
     // ** 움직임을 저장하는 벡터
     private Vector3 Movement;
 
-    // ** 플레이어의 Animator 구성요소를 받아오기 위해
-    public Animator animator;
+    // ** 플레이어의 Animator 구성요소를 받아오기위해...
+    private Animator animator;
 
-    // ** 플레이어의 SpriteRenderer 구성요소를 받아오기 위해
+    // ** 플레이어의 SpriteRenderer 구성요소를 받아오기위해...
     private SpriteRenderer playerRenderer;
-
 
     // ** [상태체크]
     private bool onAttack; // 공격상태
     private bool onHit; // 피격상태
-    private bool onRoll;
-    private bool onDie;
-    private bool onJump;
 
-    // ** 복사할 총알 원본
+    // ** 복제할 총알 원본
     private GameObject BulletPrefab;
 
-    // ** 복제할 fx 원본
+    // ** 복제할 FX 원본
     private GameObject fxPrefab;
 
-
+    // 추후 list로 변경
     public GameObject[] stageBack = new GameObject[7];
 
-
-    // ** 복제된 총알의 저장공간
+    // ** 복제된 총알의 저장공간.
     private List<GameObject> Bullets = new List<GameObject>();
 
-    // ** 플레이어가 마자막으로 바라본 방향
+    // ** 플레이어가 마지막으로 바라본 방향.
     private float Direction;
+
 
     // ** 플레이어가 바라보는 방향
     public bool DirLeft;
     public bool DirRight;
 
+
     private void Awake()
     {
         // ** player 의 Animator를 받아온다.
-        animator = GetComponent<Animator>();
+        animator = this.GetComponent<Animator>();
 
         // ** player 의 SpriteRenderer를 받아온다.
-        playerRenderer = GetComponent<SpriteRenderer>();
+        playerRenderer = this.GetComponent<SpriteRenderer>();
 
         // ** [Resources] 폴더에서 사용할 리소스를 들고온다.
         BulletPrefab = Resources.Load("Prefabs/Bullet") as GameObject;
-        fxPrefab = Resources.Load("Prefabs/Fx/Smoke") as GameObject;
+        fxPrefab = Resources.Load("Prefabs/FX/Smoke") as GameObject;
     }
 
     // ** 유니티 기본 제공 함수
-    // ** 초기값을 설정할때 사용
+    // ** 초기값을 설정할 때 사용
     void Start()
     {
-        // ** 속도를 초기화
-        Speed = 5.0f;       
+        //EnemyManager.GetInstance.CreateTest();
+
+        // ** 속도를 초기화.
+        Speed = 5.0f;
 
         // ** 초기값 셋팅
         onAttack = false;
         onHit = false;
-        onRoll = false;
-        onDie = false;
-        onJump = false;
-
         Direction = 1.0f;
 
+        DirLeft = false;
+        DirRight = false;
 
         for (int i = 0; i < 7; ++i)
             stageBack[i] = GameObject.Find(i.ToString());
     }
 
     // ** 유니티 기본 제공 함수
-    // ** 프레임마다 반복적으로 실행되는 함수
+    // ** 프레임마다 반복적으로 실행되는 함수.
     void Update()
     {
-        // ** 실수 연산 IEEE754
+        // **  Input.GetAxis =     -1 ~ 1 사이의 값을 반환함. 
+        float Hor = Input.GetAxisRaw("Horizontal"); // -1 or 0 or 1 셋중에 하나를 반환.
+        float Ver = Input.GetAxisRaw("Vertical"); // -1 or 0 or 1 셋중에 하나를 반환.
 
-        // ** Input.GetAxis = -1 ~ 1 사이의 값을 반환함.
-        float Hor = Input.GetAxisRaw("Horizontal"); // -1 or 0 or 1 셋중에 하나를 반환
-        float Ver = Input.GetAxis("Vertical"); // -1 ~ 1까지 실수로 반환 
-
-
-        // **  입력받은 값으로 플레이어를 움직인다
+        // ** 입력받은 값으로 플레이어를 움직인다.
         Movement = new Vector3(
             Hor * Time.deltaTime * Speed,
-            0.0f,
+            Ver * Time.deltaTime * Speed,
             0.0f);
 
-        // ** Hor이 0이라며 멈춰있는 상태이므로 예외처리를 해준다.
+        transform.position += new Vector3(0.0f, Movement.y, 0.0f);
+
+
+        // ** Hor이 0이라면 멈춰있는 상태이므로 예외처리를 해준다. 
         if (Hor != 0)
             Direction = Hor;
 
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            // ** 플레이어의 좌표가 0.0보다 작을때 플레이어만 움직인다.
-            if (transform.position.x < 0)
-            {
+            // ** 플레이어의 좌표가 0.1f 보다 작을때 플레이어만 움직인다.
+            if (transform.position.x < 0.1f)
                 transform.position += Movement;
-            }
-
             else
             {
                 ControllerManager.GetInstance().DirRight = true;
@@ -119,11 +114,14 @@ public class PlayerController : MonoBehaviour
         {
             ControllerManager.GetInstance().DirRight = false;
             ControllerManager.GetInstance().DirLeft = true;
-               // ** 플레이어의 좌표가 -15보다 클때 플레이어만 움직인다.
-                if(transform.position.x > -15.0f)
+
+            // ** 플레이어의 좌표가 -15.0 보다 클때 플레이어만 움직인다.
+            if (transform.position.x > -15.0f)
                 // ** 실제 플레이어를 움직인다.
                 transform.position += Movement;
         }
+
+
 
         if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
         {
@@ -131,12 +129,11 @@ public class PlayerController : MonoBehaviour
             ControllerManager.GetInstance().DirLeft = false;
         }
 
-        // ** 플레이어가 바라보고있는 방향에 따라 이미지 반전 설정
+
+        // ** 플레이어가 바라보고있는 방향에 따라 이미지 반전 설정.
         if (Direction < 0)
         {
             playerRenderer.flipX = DirLeft = true;
-            // ** 실제 플레이어를 움직인다.
-            transform.position += Movement;
         }
         else if (Direction > 0)
         {
@@ -145,25 +142,12 @@ public class PlayerController : MonoBehaviour
         }
 
 
-       
-        // ** 좌측 컨트롤키를 입력한다면
-        if (Input.GetKey(KeyCode.LeftControl))
-            OnAttack();
-
-        // ** 좌측 쉬프트키를 입력한다면
+        // ** 좌측 쉬프트키를 입력한다면.....
         if (Input.GetKey(KeyCode.LeftShift))
+            // ** 피격
             OnHit();
 
-        if (Input.GetKey(KeyCode.Z))
-            OnRoll();
-
-        if (Input.GetKey(KeyCode.X))
-            OnDie();
-
-        if (Input.GetKey(KeyCode.C))
-            OnJump();
-
-        // ** 스페이스바를 입력한다면
+        // ** 스페이스바를 입력한다면..
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // ** 공격
@@ -181,7 +165,7 @@ public class PlayerController : MonoBehaviour
             // ** 총알 스크립트내부의 방향 변수를 현재 플레이어의 방향 변수로 설정 한다.
             Controller.Direction = new Vector3(Direction, 0.0f, 0.0f);
 
-            // ** 총알 스크립트 내부의 FX Prefab을 설정한다.
+            // ** 총알 스크립트내부의 FX Prefab을 설정한다.
             Controller.fxPrefab = fxPrefab;
 
             // ** 총알의 SpriteRenderer를 받아온다.
@@ -194,10 +178,8 @@ public class PlayerController : MonoBehaviour
             Bullets.Add(Obj);
         }
 
-        // ** 플레이어의 움직임에 따라 이동 모션을 실행 한다.
+        // ** 플레이의 움직임에 따라 이동 모션을 실행 한다.
         animator.SetFloat("Speed", Hor);
-
-
     }
 
     private void OnAttack()
@@ -207,79 +189,45 @@ public class PlayerController : MonoBehaviour
             // ** 함수를 종료시킨다.
             return;
 
-        //** 함수가 종료되지 않았다면
-        // ** 공격상태를 활성화 하고
+        // ** 함수가 종료되지 않았다면...
+        // ** 공격상태를 활성화 하고.
         onAttack = true;
 
-        // ** 공격모션을 실행시킨다.
+        // ** 공격모션을 실행 시킨다.
         animator.SetTrigger("Attack");
-
     }
 
     private void SetAttack()
     {
         // ** 함수가 실행되면 공격모션이 비활성화 된다.
-        // ** 함수는 애니메이션 클립의 이벤트 프레임으로 삽입됨
+        // ** 함수는 애니매이션 클립의 이벤트 프레임으로 삽입됨.
         onAttack = false;
     }
 
     private void OnHit()
     {
+        // ** 이미 피격모션이 진행중이라면
         if (onHit)
+            // ** 함수를 종료시킨다.
             return;
 
+        // ** 함수가 종료되지 않았다면...
+        // ** 피격상태를 활성화 하고.
         onHit = true;
-        animator.SetTrigger("Hit");
 
+        // ** 피격모션을 실행 시킨다.
+        animator.SetTrigger("Hit");
     }
 
     private void SetHit()
     {
+        // ** 함수가 실행되면 피격모션이 비활성화 된다.
+        // ** 함수는 애니매이션 클립의 이벤트 프레임으로 삽입됨.
         onHit = false;
     }
 
-    private void OnRoll()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (onRoll)
-            return;
-
-        onRoll = true;
-        animator.SetTrigger("Roll");
-
+        print("Coll");
     }
-
-    private void SetRoll()
-    {
-        onRoll = false;
-    }
-
-    private void OnDie()
-    {
-        if (onDie)
-            return;
-
-        onDie = true;
-        animator.SetTrigger("Die");
-
-    }
-
-    private void SetDie()
-    {
-        onDie = false;
-    }
-
-    private void OnJump()
-    {
-        if (onJump)
-            return;
-
-        onJump = true;
-        animator.SetTrigger("Jump");
-    }
-
-    private void SetJump()
-    {
-        onJump = false;
-    }
-
 }

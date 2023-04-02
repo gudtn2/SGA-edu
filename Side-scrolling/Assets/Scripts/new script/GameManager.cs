5b343bd26c49a990f7f2ce203e1aec72af55025a
gudtn2 <tyrueu@gmail.com>
2023-04-03 06:53:39 +0900

    edu

--- Side-scrolling/Assets/Scripts/new script/GameManager.cs
+++ Side-scrolling/Assets/Scripts/new script/GameManager.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class GameManager : MonoBehaviour
 {
     public string[] enemyObjs;
     public Transform[] spawnPoints;
 
-    public float maxSpawnDelay;
+    public float nextSpawnDelay;
     public float curSpawnDelay;
 
     public GameObject player;
@@ -19,18 +20,52 @@ public class GameManager : MonoBehaviour
     public GameObject gameOverSet;
     public ObjectManager objectManager;
 
+    public List<Spawn> spawnList;
+    public int spawnIndex;
+    public bool spawnEnd;
     private void Awake()
     {
+        spawnList = new List<Spawn>();
         enemyObjs = new string[] { "EnemyS", "EnemyM", "EnemyL" };
+        ReadSpawnFile();
+    }
+    void ReadSpawnFile()
+    {
+        //#1. 변수 초기화
+        spawnList.Clear();
+        spawnIndex = 0;
+        spawnEnd = false;
+
+        //#2. 리스폰 파일 열기
+        TextAsset textFile = Resources.Load("Stage 0") as TextAsset;
+        StringReader stringReader = new StringReader(textFile.text);
+
+        while(stringReader != null)
+        {
+            string line = stringReader.ReadLine();
+            Debug.Log(line);
+            if (line == null)
+                break;
+            //#. 리스폰 데이터 생성
+            Spawn spawnData = new Spawn();
+            spawnData.delay = float.Parse(line.Split(',')[0]);
+            spawnData.type = line.Split(',')[1];
+            spawnData.point = int.Parse(line.Split(',')[2]);
+            spawnList.Add(spawnData);
+        }
+        //# 텍스트 파일 닫기
+        stringReader.Close();
+
+        //# 첫번째 스폰 딜레이 적용
+        nextSpawnDelay = spawnList[0].delay;
     }
     private void Update()
     {
         curSpawnDelay += Time.deltaTime;
 
-        if(curSpawnDelay > maxSpawnDelay)
+        if(curSpawnDelay > nextSpawnDelay && !spawnEnd)
         {
-            SpawnEnemy();
-            maxSpawnDelay = Random.Range(0.5f, 3f);
+            SpawnEnemy();            
             curSpawnDelay = 0;
         }
 
@@ -41,21 +76,34 @@ public class GameManager : MonoBehaviour
 
     void SpawnEnemy()
     {
-        int ranEnemy = Random.Range(0, 3);
-        int ranPoint = Random.Range(0, 9);
-        GameObject enemy = objectManager.MakeObj(enemyObjs[ranEnemy]);
-        enemy.transform.position = spawnPoints[ranPoint].position;
+        int enemyIndex = 0;
+        switch(spawnList[spawnIndex].type)
+        {
+            case "S":
+                enemyIndex = 0;
+                break;
+            case "M":
+                enemyIndex = 1;
+                break;
+            case "L":
+                enemyIndex = 2;
+                break;
+        }
+        
+        int enemyPoint = spawnList[spawnIndex].point;
+        GameObject enemy = objectManager.MakeObj(enemyObjs[enemyIndex]);
+        enemy.transform.position = spawnPoints[enemyPoint].position;
 
         Rigidbody2D rigid = enemy.GetComponent<Rigidbody2D>();
         Enemy enemyLogic = enemy.GetComponent<Enemy>();
         enemyLogic.player = player;
         enemyLogic.objectManager = objectManager;
 
-        if (ranPoint == 5 || ranPoint == 6) //위에서 스폰
+        if (enemyPoint == 5 || enemyPoint == 6) //위에서 스폰
         {
             rigid.velocity = new Vector2(-1, enemyLogic.speed * (-1));
         }
-        else if(ranPoint == 7 || ranPoint == 8) //밑에서 스폰
+        else if(enemyPoint == 7 || enemyPoint == 8) //밑에서 스폰
         {
             rigid.velocity = new Vector2(-1, enemyLogic.speed);
         }
@@ -64,6 +112,16 @@ public class GameManager : MonoBehaviour
             rigid.velocity = new Vector2(enemyLogic.speed * (-1) , 0);
 
         }
+
+        //#리스폰 인덱스 증가
+        spawnIndex++;
+        if(spawnIndex == spawnList.Count)
+        {
+            spawnEnd = true;
+            return;
+        }
+        //# 다음 리스폰 딜레이 갱신
+        nextSpawnDelay = spawnList[spawnIndex].delay;
     }
 
     public void UpadateLifeIcon(int life)

5ae2138ee091a4f0ee13e170f0e376873e924b23
gudtn2 <tyrueu@gmail.com>
2023-04-13 13:49:57 +0900

    0413

--- Side-scrolling/Assets/Scripts/new script/ExampleManager.cs
+++ Side-scrolling/Assets/Scripts/new script/ExampleManager.cs
@@ -1,42 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
 
-[System.Serializable]
-public class MemberForm
-{
-    public int index;
-    public string name;
-    public int age;
-    public int gender;
-
-    public MemberForm(int index, string name, int age, int gender)
-    {
-        this.index = index;
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    }
-}
-// 회원가입
-// 로그인
 
 
 public class ExampleManager : MonoBehaviour
 {
     string URL = "https://script.google.com/macros/s/AKfycbx7nzu7bazW30J6sQEsjtWvpa17RAbN04k76ffmqce0TYy2GwbV8kLeg4jXLBXoBxFf/exec";
+    public InputField IDInput, PassInput;
+    string id, pass;
 
-    IEnumerator Start()
+    bool SetIDPass()
+    {
+        id = IDInput.text.Trim();
+        pass = PassInput.text.Trim();
+
+        if (id == "" || pass == "") return false;
+        else return true;
+    }
+
+    public void Register()
+    {
+        if (!SetIDPass())
+        {
+            print("아이디 또는 비밀번호가 비어있습니다.");
+            return;
+        }
+
+        WWWForm form = new WWWForm();
+        form.AddField("order", "register");
+        form.AddField("id", id);
+        form.AddField("pass", pass);
+
+        StartCoroutine(Post(form));
+    }
+
+    public void Login()
+    {
+        if (!SetIDPass())
+        {
+            print("아이디 또는 비밀번호가 비어있습니다.");
+            return;
+        }
+
+        WWWForm form = new WWWForm();
+        form.AddField("order", "login");
+        form.AddField("id", id);
+        form.AddField("pass", pass);
+
+        StartCoroutine(Post(form));
+    }
+    IEnumerator Post(WWWForm form)
     {
         // ** 요청을 하기위한 작업
         //UnityWebRequest request = UnityWebRequest.Get(URL);
 
         //MemberForm member = new MemberForm("변사또", 45);
-
-        //WWWForm form = new WWWForm();
 
         //form.AddField("Name", member.Name);
         //form.AddField("Age", member.Age);
@@ -51,16 +74,12 @@ public class ExampleManager : MonoBehaviour
         //}
 
 
-        using (UnityWebRequest request = UnityWebRequest.Get(URL))
+        using (UnityWebRequest request = UnityWebRequest.Post(URL, form))
         {
             yield return request.SendWebRequest();
 
-            MemberForm json = JsonUtility.FromJson<MemberForm>(request.downloadHandler.text);
-            // ** 응답에 대한 작업
-            print(json.name);
-            print(json.age);
-            print(json.index);
-            print(json.gender);
+            if (request.isDone) print(request.downloadHandler.text);
+            else print("웹의 응답이 없습니다.");
         }
     }
 
